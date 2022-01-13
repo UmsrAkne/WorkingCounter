29984f89d9c402abeb9466c441f9836903cbbc32
Akn <sibata486@gmail.com>
2022-01-13 22:51:12 +0900

    add / 作業を表すクラスを追加

---
+++ WorkingCounter/Models/Work.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkingCounter.Models
+{
+    using System;
+
+    public class Work
+    {
+        public DateTime AdditionDate { get; set; }
+
+        /// <summary>
+        /// ノルマを表す 
+        /// </summary>
+        public int Quota { get; set; }
+
+        public string Unit { get; set; }
+    }
+}
---
+++ WorkingCounter/Models/WorkingUnit.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkingCounter.Models
+{
+    using System;
+
+    public class WorkingUnit
+    {
+        public DateTime AdditionDate { get; set; }
+
+        public string Memo { get; set; }
+
+        public TimeSpan WorkingDuration { get; set; }
+    }
+}
