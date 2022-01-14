8b98e20ebb5dd312f465fdbb96f8e8e3ce61ec29
Akn <sibata486@gmail.com>
2022-01-14 22:44:45 +0900

    add / entityframework と DbContext を継承したクラスを追加

---
+++ WorkingCounter/Models/DBs/WorkingDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkingCounter.Models.DBs
+{
+    using System.IO;
+    using System.Data.SQLite;
+    using Microsoft.Data.Sqlite;
+    using Microsoft.EntityFrameworkCore;
+
+    public class WorkingDbContext : DbContext
+    {
+        public DbSet<Work> Works { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!File.Exists("workingdb"))
+            {
+                SQLiteConnection.CreateFile("workingdb.sqlite"); // ファイルが存在している場合は問答無用で上書き。
+            }
+
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = @"workingdb.sqlite" }.ToString();
+            optionsBuilder.UseSqlite(new SQLiteConnection(connectionString));
+        }
+    }
+}
