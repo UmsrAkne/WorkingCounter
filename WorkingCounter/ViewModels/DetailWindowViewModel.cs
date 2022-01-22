0fcfbee0575b1b7bea8a6e3b8d787797aef18979
Akn <sibata486@gmail.com>
2022-01-22 23:50:34 +0900

    add / 詳細のウィンドウを出すためのクラスとコードを追加

---
+++ WorkingCounter/ViewModels/DetailWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace WorkingCounter.ViewModels
+{
+    using System;
+    using Prism.Services.Dialogs;
+
+    public class DetailWindowViewModel : IDialogAware
+    {
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title => "Detail window";
+
+        public bool CanCloseDialog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
--- WorkingCounter/ViewModels/MainWindowViewModel.cs
+++ WorkingCounter/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@
     using System.Windows.Controls;
     using Prism.Commands;
     using Prism.Mvvm;
+    using Prism.Services.Dialogs;
     using WorkingCounter.Models;
     using WorkingCounter.Models.DBs;
 
@@ -14,13 +15,16 @@
         private ObservableCollection<Work> works;
         private WorkingDbContext workingDbContext;
         private string inputText;
+        private IDialogService dialogService;
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IDialogService dialogService)
         {
             Works = new ObservableCollection<Work>();
             workingDbContext = new WorkingDbContext();
             workingDbContext.CreateDatabase();
             ReloadWorks();
+
+            this.dialogService = dialogService;
         }
 
         public DelegateCommand AddWorkCommand => new DelegateCommand(() =>
@@ -49,6 +53,12 @@
                 workingDbContext.Insert(unit);
                 ReloadWorks();
             }
+        });
+
+        public DelegateCommand ShowDetailWindowCommand => new DelegateCommand(() =>
+        {
+            var param = new DialogParameters();
+            dialogService.ShowDialog(nameof(DetailWindow), param, (IDialogResult result) => { });
         });
 
         public string Title
