d9d0dd4b0d82c3790d63f575d8f8010a1599fb88
Akn <sibata486@gmail.com>
2022-01-24 23:49:11 +0900

    add / DetailWindow の ListView のソースと WorkingUnit をバインド

--- WorkingCounter/ViewModels/DetailWindowViewModel.cs
+++ WorkingCounter/ViewModels/DetailWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace WorkingCounter.ViewModels
 {
     using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
     using Prism.Mvvm;
     using Prism.Services.Dialogs;
     using WorkingCounter.Models;
@@ -8,12 +10,15 @@
     public class DetailWindowViewModel : BindableBase, IDialogAware
     {
         private Work work;
+        private ObservableCollection<WorkingUnit> workingUnits;
 
         public event Action<IDialogResult> RequestClose;
 
         public string Title => "Detail window";
 
         public Work Work { get => work; set => SetProperty(ref work, value); }
+
+        public ObservableCollection<WorkingUnit> WorkingUnits { get => workingUnits; set => SetProperty(ref workingUnits, value); }
 
         public bool CanCloseDialog() => true;
 
@@ -24,7 +29,7 @@
         public void OnDialogOpened(IDialogParameters parameters)
         {
             Work = parameters.GetValue<Work>(nameof(Work));
-            System.Diagnostics.Debug.WriteLine(Work);
+            WorkingUnits = new ObservableCollection<WorkingUnit>(Work.Units.ToList());
         }
     }
 }
