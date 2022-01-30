67438b07e5b878ea9d717f7596975050464f07c8
Akn <sibata486@gmail.com>
2022-01-30 23:28:43 +0900

    add / ウィンドウのテキストボックスとプロパティをバインド

--- WorkingCounter/ViewModels/WorkAdditionWindowViewModel.cs
+++ WorkingCounter/ViewModels/WorkAdditionWindowViewModel.cs
@@ -1,13 +1,59 @@
 ﻿namespace WorkingCounter.ViewModels
 {
     using System;
+    using Prism.Mvvm;
     using Prism.Services.Dialogs;
+    using WorkingCounter.Models;
 
-    public class WorkAdditionWindowViewModel : IDialogAware
+    public class WorkAdditionWindowViewModel : BindableBase, IDialogAware
     {
+        private Work work = new Work();
+        private string name;
+        private int dateCountToLimit;
+        private int dateCountToStart;
+        private int quota;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => "New work window";
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                SetProperty(ref name, value);
+                work.Name = value;
+            }
+        }
+
+        public int DateCountToLimit
+        {
+            get => dateCountToLimit;
+            set
+            {
+                SetProperty(ref dateCountToLimit, value);
+            }
+        }
+
+        public int DateCountToStart
+        {
+            get => dateCountToStart;
+            set
+            {
+                SetProperty(ref dateCountToStart, value);
+            }
+        }
+
+        public int Quota
+        {
+            get => quota;
+            set
+            {
+                SetProperty(ref quota, value);
+                work.Quota = value;
+            }
+        }
 
         public bool CanCloseDialog()
         {
